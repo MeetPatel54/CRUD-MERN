f54e4616e3432c3625fbfa58d4575f6c4990e285
Pmu782005 <pmeet782005@gmail.com>
2025-06-17 13:43:37 +0530

    MongodbConnected

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("DB connected successfully.");
+    app.listen(PORT, () => {
+      console.log(`server running on : ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
