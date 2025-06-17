c244522f76971b7f2d1213458c22c31aebb5735a
Pmu782005 <pmeet782005@gmail.com>
2025-06-17 15:00:30 +0530

    data Deleted

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -58,3 +58,17 @@ export const update = async (req, res) => {
     res.status(500).json({ errorMessage: err.message });
   }
 };
+
+export const deleteUser = async(req,res) => {
+    try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(404).json({ message: "User Not Found" });
+    }
+    await User.findByIdAndDelete(id);
+    res.status(200).json({message:"User deleted successfully"});
+  } catch (err) {
+    res.status(500).json({ errorMessage: err.message });
+  }
+}
